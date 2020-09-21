50425f2b77ded908df874f903c9bdf77b618e86c
ageek19 <amg2896@gmail.com>
2020-09-21 08:35:48 +0300

    Updates

---
+++ precache-manifest.3fc909b4482733f1677a7b472d72f3ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "524b1403bff34901be7fc5a4de0ae021",
+    "url": "/js-demo-react-app/index.html"
+  },
+  {
+    "revision": "da52f94efc9535df1ea7",
+    "url": "/js-demo-react-app/static/css/main.f067383d.chunk.css"
+  },
+  {
+    "revision": "3d577a6c509fde56909c",
+    "url": "/js-demo-react-app/static/js/2.137014c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/js-demo-react-app/static/js/2.137014c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da52f94efc9535df1ea7",
+    "url": "/js-demo-react-app/static/js/main.cb6b24bb.chunk.js"
+  },
+  {
+    "revision": "7f9ea526f2d95ab6b7f4",
+    "url": "/js-demo-react-app/static/js/runtime-main.4618d029.js"
+  },
+  {
+    "revision": "8d9fe1e535063026e952d3d3a2f619a7",
+    "url": "/js-demo-react-app/static/media/rick-morty-logo.8d9fe1e5.png"
+  }
+]);
