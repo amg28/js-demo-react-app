1acd2c9d2d6f57b17116fd3850915867c20ae54b
ageek19 <amg2896@gmail.com>
2020-10-02 02:09:03 +0300

    Updates

---
+++ precache-manifest.0efdd24d7f359411d20a2dc3c7938d79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c7eba2f00599de31215039669b58e76",
+    "url": "/js-demo-react-app/index.html"
+  },
+  {
+    "revision": "5e83a4ebe722a532ffef",
+    "url": "/js-demo-react-app/static/css/main.e146f283.chunk.css"
+  },
+  {
+    "revision": "7710f077cc3cd4470844",
+    "url": "/js-demo-react-app/static/js/2.cf6bf28a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/js-demo-react-app/static/js/2.cf6bf28a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e83a4ebe722a532ffef",
+    "url": "/js-demo-react-app/static/js/main.81424c88.chunk.js"
+  },
+  {
+    "revision": "7f9ea526f2d95ab6b7f4",
+    "url": "/js-demo-react-app/static/js/runtime-main.4618d029.js"
+  },
+  {
+    "revision": "46971e1185254396efce7b908d644e69",
+    "url": "/js-demo-react-app/static/media/rick-morty-logo.46971e11.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/js-demo-react-app/precache-manifest.5829ec4cca107a8a30c2f18476dcc10c.js"
+  "/js-demo-react-app/precache-manifest.0efdd24d7f359411d20a2dc3c7938d79.js"
 );
 
 self.addEventListener('message', (event) => {
