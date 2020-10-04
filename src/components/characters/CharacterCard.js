c38136589953e25dd4494f8fdbb9ab95d150e59f
ageek19 <amg2896@gmail.com>
2020-10-04 21:55:30 +0300

    Updated card-details view

--- src/components/characters/CharacterCard.js
+++ src/components/characters/CharacterCard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Card.css';
 
 const CharacterCard = ({data}) => {
 
@@ -6,16 +7,22 @@ const CharacterCard = ({data}) => {
     console.log(name);
 
     return (
-        <div className="card">
-            <img className="card-img" src={image} alt="{name}"></img>
-            <div className="card-name">{name}</div>
-            <div className="card-details">
-                <div>Gender: {gender}</div>
-                <div>Location: {location.name}</div>
-                <div>Origin: {origin.name}</div>
-                <div>Species: {species}</div>
-                <div>Status: {status}</div>
-                <div>Episodes: {episode.length}</div>
+        <div className="card-container">
+            <div className="card">
+                <div className="card-front">
+                    <img className="card-img" src={image} alt="{name}"></img>
+                    <div className="card-name">{name}</div>
+                </div>
+                <div className="card-back">
+                    <div className="card-details">
+                        <div>Gender: {gender}</div>
+                        <div>Location: {location.name}</div>
+                        <div>Origin: {origin.name}</div>
+                        <div>Species: {species}</div>
+                        <div>Status: {status}</div>
+                        <div>Episodes: {episode.length}</div>
+                    </div>
+                </div>
             </div>
         </div>
     )
